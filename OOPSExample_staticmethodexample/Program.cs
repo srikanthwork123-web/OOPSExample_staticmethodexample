1998f4f0b94b198833016f1f5a606646a39f7c57
Dell <Dell@DESKTOP-13B42NJ>
2026-01-19 09:05:18 +0530

    Add project files.

---
+++ OOPSExample_staticmethodexample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_staticmethodexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //to access the static varibales or static methods in outside class
+            //static method calling syntax:classname.methodname    
+            employee.displaycompanydetails();
+
+
+            //to access the normal methods outside
+            //normal method synatx:classname objectname=new classname();
+            employee objectname = new employee();
+            objectname.displayempdateils();
+        }
+    }
+}
---
+++ OOPSExample_staticmethodexample/employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_staticmethodexample
+{
+    internal class employee
+    {
+        public int employeeNumber;//non static variable or normal variable
+        public static string companyname;//static variable
+        public static string companylocation;//static variable
+        static employee()
+        {//To assign the data for static variables we will use static constuctor.
+            companyname = "wipro";
+            companylocation = "hyderabad";
+           // employeeNumber = 100;//you can't assign data to normal variable in static constuctor.
+        }
+        public employee()
+        {
+            this.employeeNumber = 10;
+        }
+        public static void displaycompanydetails()//static method
+        {
+            Console.WriteLine("company name is:" + companyname);
+            Console.WriteLine("company location is:" + companylocation);
+        }
+        public void displayempdateils()
+        {
+            Console.WriteLine("employeenumber is" + employeeNumber);
+        }
+    }
+}
