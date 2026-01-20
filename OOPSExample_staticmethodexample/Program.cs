6313e603055f72fbb0fb558a9934961c263574cb
Dell <Dell@DESKTOP-13B42NJ>
2026-01-20 08:13:43 +0530

    Commented lines added for understanding purpose

--- OOPSExample_staticmethodexample/Program.cs
+++ OOPSExample_staticmethodexample/Program.cs
@@ -9,16 +9,23 @@ namespace OOPSExample_staticmethodexample
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //to access the static varibales or static methods in outside class
-            //static method calling syntax:classname.methodname    
+        {//=========================calling static varibales /static methods in outside class ==============================
+            //to access the static varibales or static methods in outside class,no need to create a object
+            //static method calling syntax:classname.methodname();
+            //static variable calling syntax:classname.variablename;
             employee.displaycompanydetails();
+            employee.companyname = "sampledata";
+            employee.companylocation = "hyderabad";
+            employee.displaycompanydetails();//we can call methods multiple times also.based on your requirment.
+//=========================calling non static varibales /non static methods in outside class ==============================
+            //to access  normal methods(without static) in  outsideclass,we need to create a object
+            //object creation syntax:classname objectname=new classname();
+            //non static or instance method calling synatx:objectname.methodname();
+            //non static or instance variable calling synatx:objectname.variablename;
 
-
-            //to access the normal methods outside
-            //normal method synatx:classname objectname=new classname();
-            employee objectname = new employee();
-            objectname.displayempdateils();
+            employee objectname = new employee();//created object for employee class.for accessing non static items/members outside.
+            objectname.displayempdateils();//access the non static method using objectname.methodname();
+            objectname.employeeNumber = 1000;//access the non static variable using objectname.variablename;
         }
     }
 }
--- OOPSExample_staticmethodexample/employee.cs
+++ OOPSExample_staticmethodexample/employee.cs
@@ -7,28 +7,40 @@ using System.Threading.Tasks;
 namespace OOPSExample_staticmethodexample
 {
     internal class employee
-    {
-        public int employeeNumber;//non static variable or normal variable
+    {//Note:If you want to access static members outside no need to create a object
+        public int employeeNumber;//non static variable or normal/instance variable or fields
         public static string companyname;//static variable
         public static string companylocation;//static variable
-        static employee()
+        static employee()//static constructor=>before classname() if u use static keyword.we called this as static constructor.
         {//To assign the data for static variables we will use static constuctor.
             companyname = "wipro";
             companylocation = "hyderabad";
-           // employeeNumber = 100;//you can't assign data to normal variable in static constuctor.
+            //employeeNumber = 100;//you can't assign data to normal variable in static constuctor.it will throe compiletime error..
         }
-        public employee()
-        {
+        public employee()//non static constructor or instance constructor or normal constuctor.
+        {//To assign the data to non static variables we will use normal or instance constructor.
             this.employeeNumber = 10;
         }
-        public static void displaycompanydetails()//static method
+        /*methodsynatx:
+        accessmodifier qualifier returntype methodname(arguments or parameters)
         {
+          return data;
+        }
+        Note:qualifier  means static...this is optional
+         */
+        public static void displaycompanydetails()//static method
+        {//static method is used to acces or dispaly only static variables data  only.
             Console.WriteLine("company name is:" + companyname);
             Console.WriteLine("company location is:" + companylocation);
+            //Console.WriteLine("company location is:" + employeeNumber);//u can't use normalor non static  variables in static method.it will throw compiletime error.
         }
-        public void displayempdateils()
+        public void displayempdateils()//non static method or instance method or normal method.
         {
+            Console.WriteLine("company name is:" + companyname);//we can access static variables in non static method
+            Console.WriteLine("company location is:" + companylocation);//it will not throw any compiletime error
             Console.WriteLine("employeenumber is" + employeeNumber);
         }
     }
 }
+//Normal/instance/static/non static variables we can access and call in normal/non static method.
+//but in static constuctor or static method we can access only static related things only.(non static memebers we can't access.it will throw compiletime error)
